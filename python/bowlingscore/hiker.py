bfd9242f5d805152e1e19cead80a2d5f592ed3f7
Paul Moore <pdmoore@gmail.com>
2021-09-15 15:23:07 -0700

    python - unclean impl passes all tests

--- python/bowlingscore/hiker.py
+++ python/bowlingscore/hiker.py
@@ -1,47 +1,51 @@
 class Hiker(object):
 
     def __init__(self, rolls):
-        self.rolls = rolls
+        self.r = rolls
+        self.r2 = []
+        for i in range(0, len(self.r)):
+            if self.r[i] == 10:
+                self.r2.append(0)
+            self.r2.append(self.r[i])
+        self.r = self.r2
 
     def score(self):
-
         y = 0
-
-        for i in range(0, len(self.rolls)):
-            if self.rolls[i] != 10:
+        for i in range(0, len(self.r)):
+            if self.r[i] != 10:
                 if i > 0 and i % 2 == 1:
-                    y2 = self.rolls[i] + self.rolls[i - 1]
+                    y2 = self.r[i] + self.r[i - 1]
                     if y2 == 10:
-                        y += self.rolls[i + 1]
+                        y += self.r[i + 1]
             #               if (y2 == 10) {
             #               y = self.rolls[i +1];
-            elif self.rolls[i] == 10:
+            elif self.r[i] == 10:
                 if i <= 16:
-                    if self.rolls[i + 2] != 10:
-                        y += self.rolls[i + 1]
-                        y += self.rolls[i + 2]
+                    if self.r[i + 2] != 10:
+                        y += self.r[i + 1]
+                        y += self.r[i + 2]
                     else:
-                        y += self.rolls[i + 2]
-                        if self.rolls[i + 4] < 10:
-                            y += self.rolls[i + 3]
+                        y += self.r[i + 2]
+                        if self.r[i + 4] < 10:
+                            y += self.r[i + 3]
                         else:
-                            y += self.rolls[i + 4]
+                            y += self.r[i + 4]
                 else:
                     if i == 17:
-                        if self.rolls[i + 2] != 10:
-                            y += self.rolls[i + 1]
-                            y += self.rolls[i + 2]
+                        if self.r[i + 2] != 10:
+                            y += self.r[i + 1]
+                            y += self.r[i + 2]
                         else:
-                            y += self.rolls[i + 2]
-                            y += self.rolls[i + 3]
+                            y += self.r[i + 2]
+                            y += self.r[i + 3]
                     else:
-                        y += self.rolls[i]
+                        y += self.r[i]
                         # y += self.rolls[i+2]
                         # y += 10
             #            else:
             #              y += self.rolls[i+1];
             #              y += self.rolls[i+2];
             if i < 20:
-                y += self.rolls[i]
+                y += self.r[i]
 
         return y
--- python/bowlingscore/test_hiker.py
+++ python/bowlingscore/test_hiker.py
@@ -44,33 +44,30 @@ def test_partial_game_spares_in_both_frames_1_and_2():
 
 
 def test_partial_game_strike_in_first_frame_followed_by_gutter_balls():
-    rolls = [0,10, 0,0]
+    rolls = [10, 0,0]
     sut = Hiker(rolls)
     assert sut.score() == 10
 
 
 def test_partial_game_strike_in_first_frame_next_two_rolls_added_to_strike():
-    rolls = [0,10, 7,2]
+    rolls = [10, 7,2]
     sut = Hiker(rolls)
     assert sut.score() == 28
 
 
 def test_partial_game_strike_in_third_frame_rolls_in_frame_4_added_to_strike():
-    rolls = [6,3, 0,10, 7,2]
+    rolls = [6,3, 10, 7,2]
     sut = Hiker(rolls)
     assert sut.score() == 37
 
 
 def test_partial_game_three_strikes_followed_by_gutter_balls():
-    rolls = [0,10, 0,10, 0,10, 0,0]
+    rolls = [10, 10, 10, 0,0]
     sut = Hiker(rolls)
     assert sut.score() == 60
 
 
 def test_full_game_perfect_game_scores_300():
-    rolls = [0,10, 0,10, 0,10, 0,10, 0,10, 0,10, 0,10, 0,10, 0,10, 0,10, 0,10,0,10]
+    rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,10]
     sut = Hiker(rolls)
     assert sut.score() == 300
-
-
-# The java version probably uses 10,0 to represent a strike, and all the indexes assume that blank slot
