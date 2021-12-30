dd29e4b18e8fe69d7f4560414855a931e1bc35b5
Paul Moore <pdmoore@gmail.com>
2021-12-30 15:22:21 -0800

    bowling-score implemented in c#/xunit

---
+++ csharp-xunit/bowling-score/BowlingGame.Tests/BowlingGameTest.cs
@@ -0,0 +1,181 @@
+using Xunit;
+using BowlingGame;
+
+namespace BowlingGame.Tests;
+
+public class BowlingGameTest
+{
+    [Fact]
+    public void fullGame_allGutterBalls()
+    {
+        var bowlingGame = new BowlingGame();
+        rollSomeBalls(bowlingGame, 0);
+
+        Assert.Equal(0, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void fullGame_eachFrame_onlyOnePinKnockedDown() {
+        var bowlingGame = new BowlingGame();
+        rollSomeBalls(bowlingGame, 1);
+        Assert.Equal(20, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void fullGame_eachFrame_threePinsKnockedDown() {
+        var bowlingGame = new BowlingGame();
+        rollSomeBalls(bowlingGame, 3);
+        Assert.Equal(60, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void partialGame_noSpareOrStrike_differentNumberOfPinsKnockedDownEachRoll() {
+        var bowlingGame = new BowlingGame();
+        bowlingGame.Bowl(1);
+        bowlingGame.Bowl(5);
+        bowlingGame.Bowl(6);
+        bowlingGame.Bowl(3);
+        Assert.Equal(15, bowlingGame.Score());
+    }
+    
+    [Fact]
+    public void partialGame_spareInFirstFrame_followedByGutterBalls() {
+        var bowlingGame = new BowlingGame();
+        bowlingGame.Bowl(7);
+        bowlingGame.Bowl(3);
+        bowlingGame.Bowl(0);
+        Assert.Equal(10, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void partialGame_spare_nextRollIsAddedToSpare() {
+        var bowlingGame = new BowlingGame();
+        bowlingGame.Bowl(9);
+        bowlingGame.Bowl(1);
+        bowlingGame.Bowl(4);
+        Assert.Equal(18, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void partialGame_spares_inBothFrame1And2() {
+        var bowlingGame = new BowlingGame();
+        bowlingGame.Bowl(7);
+        bowlingGame.Bowl(3);
+        bowlingGame.Bowl(9);
+        bowlingGame.Bowl(1);
+        bowlingGame.Bowl(9);
+        Assert.Equal(47, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void partialGame_strikeInFirstFrame_followedByGutterBalls() {
+        var bowlingGame = new BowlingGame();
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(0);
+        bowlingGame.Bowl(0);
+        Assert.Equal(10, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void partialGame_strikeInFirstFrame_nextTwoRollsAddedToStrike() {
+        var bowlingGame = new BowlingGame();
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(7);
+        bowlingGame.Bowl(2);
+        Assert.Equal(28, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void partialGame_strikeIn2ndFrame_rollsInFrame3AddedToStrike() {
+        var bowlingGame = new BowlingGame();
+        bowlingGame.Bowl(6);
+        bowlingGame.Bowl(3);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(7);
+        bowlingGame.Bowl(2);
+        Assert.Equal(37, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void partialGame_twoStrikes_FollowedByGutterBalls_SeconStrikeAddedToFirst_andCountedOnItsOwn() {
+        var bowlingGame = new BowlingGame();
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(0);
+        bowlingGame.Bowl(0);
+        Assert.Equal(30, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void partialGame_threeStrikes_FollowedByGutterBalls() {
+        var bowlingGame = new BowlingGame();
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(0);
+        bowlingGame.Bowl(0);
+        Assert.Equal(60, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void fullGame_PerfectGame() {
+        var bowlingGame = new BowlingGame();
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+        bowlingGame.Bowl(10);
+
+        Assert.Equal(300, bowlingGame.Score());
+    }
+
+    [Fact]
+    public void fullGame_Example_in_Readme() {
+        var bowlingGame = new BowlingGame();
+        bowlingGame.Bowl(10);
+
+        bowlingGame.Bowl(9);
+        bowlingGame.Bowl(1);
+
+        bowlingGame.Bowl(5);
+        bowlingGame.Bowl(5);
+
+        bowlingGame.Bowl(7);
+        bowlingGame.Bowl(2);
+
+        bowlingGame.Bowl(10);
+
+        bowlingGame.Bowl(10);
+
+        bowlingGame.Bowl(10);
+
+        bowlingGame.Bowl(9);
+        bowlingGame.Bowl(0);
+
+        bowlingGame.Bowl(8);
+        bowlingGame.Bowl(2);
+
+        bowlingGame.Bowl(9);
+        bowlingGame.Bowl(1);
+        bowlingGame.Bowl(10);
+
+        Assert.Equal(187, bowlingGame.Score());
+    }
+    
+    private void rollSomeBalls(BowlingGame bowlingGame, int b)
+    {
+        //TODO - doesn't check b is valid
+        //TODO - doesn't work when b>=5
+        for (int i = 0; i < 10; i++) {
+            bowlingGame.Bowl(b);
+            bowlingGame.Bowl(b);
+        }
+    }
+}
---
+++ csharp-xunit/bowling-score/BowlingGame/BowlingGame.cs
@@ -0,0 +1,72 @@
+﻿namespace BowlingGame;
+
+public class BowlingGame
+{
+
+    private int[] p = new int[22];
+    private int x = 0;
+    
+    public void Bowl(int pinsKnockedDown)
+    {
+        if (pinsKnockedDown >= 10) {
+            x++;
+        }
+        if (x < 22) {
+            this.p[x++] = pinsKnockedDown;
+        }    
+    }
+
+    public int Score()
+    {
+        int y = 0;
+        for (int i = 0; i < p.Length; i++) {
+            if (p[i] != 10) {
+                if (i > 0 && i % 2 == 1) {
+                    int y2 = p[i] + p[i - 1];
+                    if (y2 == 10) {
+                        y += p[i + 1];
+                    }
+                    //if (y2 == 10) {
+                    //  y = p[i +1];
+                    //}
+                }
+            } else if (p[i] == 10) {
+                if (i <= 16) {
+                    if (p[i + 2] != 10) {
+                        y += p[i + 1];
+                        y += p[i + 2];
+                    } else {
+                        y += p[i + 2];
+                        if (p[i + 4] < 10) {
+                            y += p[i + 3];
+                        } else {
+                            y += p[i + 4];
+                        }
+                    }
+                } else {
+                    if (i == 17) {
+                        if (p[i + 2] != 10) {
+                            y += p[i + 1];
+                            y += p[i + 2];
+                        } else {
+                            y += p[i + 2];
+                            y += p[i + 3];
+                        }
+                        y += p[i];
+                    } else {
+                        y += p[i];
+//                        y += p[i+2];
+//                        y += 10;
+                    }
+                }
+            } else {
+                // y += p[i+1];
+                // y += p[i+2];
+            }
+            if (i < 20) {
+                y += p[i];
+            }
+        }
+        return y;
+    }
+}
