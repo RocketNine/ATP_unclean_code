877dbcb963dd9021ffdd7f700712c37fa88aca56
Paul Moore <pdmoore@gmail.com>
2022-06-26 12:45:00 -0700

    ts-jest: in progress

--- typescript-jest/bowlingscore/hiker.js
+++ typescript-jest/bowlingscore/hiker.js
@@ -2,6 +2,7 @@
 
 class Hiker {
 
+    /** @param {number[]} rolls */
     constructor(rolls) {
         this.r = rolls;
         this.r2 = [];
@@ -23,41 +24,41 @@ class Hiker {
     }
 
     score() {
-        this.y = 0;
+        let y = 0;
 
         for (let i = 0; i < this.r.length; i++) {
             if (this.r[i] != 10) {
                 if ((i > 0) && (i % 2 == 1)) {
                     this.y2 = this.r[i] + this.r[i - 1];
                     if (this.y2 == 10) {
-                        this.y += this.r[i + 1];
+                        y += this.r[i + 1];
                     }
                 }
             } else if (this.r[i] == 10) {
 
                 if (i <= 16) {
                     if (this.r[i + 2] != 10) {
-                        this.y += this.r[i + 1];
-                        this.y += this.r[i + 2];
+                        y += this.r[i + 1];
+                        y += this.r[i + 2];
                     } else {
-                        this.y += this.r[i + 2];
+                        y += this.r[i + 2];
                         if (this.r[i + 4] < 10) {
-                            this.y += this.r[i + 3];
+                            y += this.r[i + 3];
                         } else {
-                            this.y += this.r[i + 4];
+                            y += this.r[i + 4];
                         }
                     }
                 } else {
                     if (i == 17) {
                         if (this.r[i + 2] != 10) {
-                            this.y += this.r[i + 1];
-                            this.y += this.r[i + 2];
+                            y += this.r[i + 1];
+                            y += this.r[i + 2];
                         } else {
-                            this.y += this.r[i + 2];
-                            this.y += this.r[i + 3];
+                            y += this.r[i + 2];
+                            y += this.r[i + 3];
                         }
                     } else {
-                        this.y += this.r[i];
+                        y += this.r[i];
                         // y += this.rolls[i + 2]
                         // y += 10
                         //#            else
@@ -66,13 +67,13 @@ class Hiker {
                     }
                 }
                 //              if(this.y2 == 10)
-                //              this.y = this.rolls[i + 1];
+                //              y = this.rolls[i + 1];
             }
             if (i < 20) {
-                this.y += this.r[i];
+                y += this.r[i];
             }
         }
-        return this.y;
+        return y;
     }
 }
 
