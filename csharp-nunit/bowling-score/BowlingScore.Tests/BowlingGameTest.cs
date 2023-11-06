f3837673e9d42a82b16449289740aa0ec8a0c72f
Paul Moore <pdmoore@gmail.com>
2023-11-05 17:14:20 -0800

    C#/NUnit - correct type in test name

--- csharp-nunit/bowling-score/BowlingScore.Tests/BowlingGameTest.cs
+++ csharp-nunit/bowling-score/BowlingScore.Tests/BowlingGameTest.cs
@@ -96,7 +96,7 @@ public class BowlingGameTest
     }
 
     [Test]
-    public void partialGame_twoStrikes_FollowedByGutterBalls_SeconStrikeAddedToFirst_andCountedOnItsOwn() {
+    public void partialGame_twoStrikes_FollowedByGutterBalls_SecondStrikeAddedToFirst_andCountedOnItsOwn() {
         var bowlingGame = new BowlingGame();
         bowlingGame.Bowl(10);
         bowlingGame.Bowl(10);
