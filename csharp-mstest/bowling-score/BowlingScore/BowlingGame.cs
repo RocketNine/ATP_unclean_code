58e30a70d0840a0f9fc0ad2106f44dc85760e359
Paul Moore <pdmoore@gmail.com>
2024-11-02 13:33:25 -0700

    c#/nunit - static import fixes problem seen in cyber-dojo

--- csharp-mstest/bowling-score/BowlingScore/BowlingGame.cs
+++ csharp-mstest/bowling-score/BowlingScore/BowlingGame.cs
@@ -1,4 +1,6 @@
-﻿namespace BowlingScore;
+﻿using static System.Math;
+
+namespace BowlingScore;
 
 public class BowlingGame
 {
@@ -39,7 +41,7 @@ public class BowlingGame
                     int y2 = p[i] + p[i - 1];
                     if (y2 == 10)
                     {
-                        y += Math.Max(0, p[i + 1]);
+                        y += Max(0, p[i + 1]);
                     }
                     //if (y2 == 10) {
                     //  y = p[i +1];
@@ -54,28 +56,28 @@ public class BowlingGame
                     {
                         if (p[i + 2] != 10)
                         {
-                            y += Math.Max(0, p[i + 1]);
+                            y += Max(0, p[i + 1]);
                         }
                         else
                         {
-                            y += Math.Max(0, p[i + 2]);
+                            y += Max(0, p[i + 2]);
                         }
                     }
                     else if (p[i + 2] != 10)
                     {
-                        y += Math.Max(0, p[i + 1]);
-                        y += Math.Max(0, p[i + 2]);
+                        y += Max(0, p[i + 1]);
+                        y += Max(0, p[i + 2]);
                     }
                     else
                     {
                         y += p[i + 2];
                         if (p[i + 4] < 10)
                         {
-                            y += Math.Max(0, p[i + 3]);
+                            y += Max(0, p[i + 3]);
                         }
                         else
                         {
-                            y += Math.Max(0, p[i + 4]);
+                            y += Max(0, p[i + 4]);
                         }
                     }
                 }
@@ -131,7 +133,7 @@ public class BowlingGame
 
             if (i < 20)
             {
-                y += Math.Max(0, p[i]);
+                y += Max(0, p[i]);
             }
         }
 
